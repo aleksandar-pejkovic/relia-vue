135e1bda84c3dcc797c603934f3cb7da487c981f
aleksandar-pejkovic <aleksandar.pejkovic89@gmail.com>
2023-05-28 19:14:29 +0200

    set server url

--- src/stores/baseUrl.js
+++ src/stores/baseUrl.js
@@ -2,7 +2,8 @@ import { defineStore } from 'pinia'
 
 export const useBaseUrlStore = defineStore('baseUrl', {
   state: () => ({
-    baseUrl: 'http://localhost:8080'
+    // baseUrl: 'http://localhost:8080'
+    baseUrl: 'https://reliabill-production.up.railway.app'
   }),
   actions: {
     getUrl(endpoint) {

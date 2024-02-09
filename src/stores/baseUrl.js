31479d026ffc4d8bc30d8804125c00d09f19e119
aleksandar-pejkovic <118291759+aleksandar-pejkovic@users.noreply.github.com>
2024-02-09 11:55:46 +0100

    Update baseUrl.js

--- src/stores/baseUrl.js
+++ src/stores/baseUrl.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useBaseUrlStore = defineStore('baseUrl', {
   state: () => ({
     // baseUrl: 'http://localhost:8080'
-    baseUrl: 'https://relia-bill-f3851f572214.herokuapp.com/'
+    baseUrl: 'http://reliabill.com/'
   }),
   actions: {
     getUrl(endpoint) {
